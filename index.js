15773a7961e461b8310212c68e815c45ad8dbc76
sumn <dltjsals6356@gmail.com>
2020-02-20 07:14:56 +0900

    make first page

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log ('Hi');
+const express = require('express'); 
+const app = express(); 
+
+
+const port = 4000;
+function handleListening() {
+    console.log(`Listening on: http://localhost:${port}`);
+}
+function handleHome(req, res){
+    res.send('hello from home');
+}
+function handleProfile(req, res){
+    res.send('you are on my profile');
+}
+app.get("/", handleHome);
+app.get('/profile', handleProfile);
+app.listen(port, handleListening); 
