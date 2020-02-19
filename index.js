289ff0d4b28da0928fb243e165f5d433b5e7bc4c
sumn <dltjsals6356@gmail.com>
2020-02-20 07:24:41 +0900

    install nodemon - automatically restart server

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express'); 
-const app = express(); 
-
-
+import express from "express";
+const app = express();
 const port = 4000;
-function handleListening() {
-    console.log(`Listening on: http://localhost:${port}`);
-}
-function handleHome(req, res){
-    res.send('hello from home');
-}
-function handleProfile(req, res){
-    res.send('you are on my profile');
-}
+
+
+
+const handleListening = () =>console.log(`Listening on: http://localhost:${port}`);
+const handleHome = (req, res) => res.send('hello from home');
+const handleProfile = (req, res) =>  res.send('you are on my profile');
+
 app.get("/", handleHome);
 app.get('/profile', handleProfile);
 app.listen(port, handleListening); 
