652af0e3109bb2d2a23c8a95a103634aa1457638
sumn <dltjsals6356@gmail.com>
2020-02-20 07:36:01 +0900

    about middleware (basic)

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const port = 4000;
 const handleListening = () =>console.log(`Listening on: http://localhost:${port}`);
 const handleHome = (req, res) => res.send('hello from home');
 const handleProfile = (req, res) =>  res.send('you are on my profile');
+const betweenHome = (req, res, next) => {
+    console.log('Between');
+    next();
+}
 
-app.get("/", handleHome);
+
+
+
+
+app.get("/", betweenHome, handleHome);
+app.use(betweenHome);
 app.get('/profile', handleProfile);
 app.listen(port, handleListening); 
