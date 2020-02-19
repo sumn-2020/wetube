1d032b84de72dc9b66205342718f65e35febad98
sumn <dltjsals6356@gmail.com>
2020-02-20 08:05:30 +0900

    add morgan(for middleware), helmet(for security), cookie-parser/body-parser

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookeParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 4000;
-
-
 
 const handleListening = () =>console.log(`Listening on: http://localhost:${port}`);
 const handleHome = (req, res) => res.send('hello from home');
 const handleProfile = (req, res) =>  res.send('you are on my profile');
-const betweenHome = (req, res, next) => {
-    console.log('Between');
-    next();
+
+
+app.use(cookeParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(helmet());
+app.use(morgan("combined"));
+
+const middleware = (req, res, next) => {
+    res.send("not happening");
 }
 
 
-
-
-
-app.get("/", betweenHome, handleHome);
-app.use(betweenHome);
+app.get("/",  handleHome);
 app.get('/profile', handleProfile);
 app.listen(port, handleListening); 
+
