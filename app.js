5acda96ce4f15c6c8670258c803a8c3471504f8a
sumn <dltjsals6356@gmail.com>
2020-02-21 01:30:01 +0900

    [Router] seperate routers into the related router
    seperate routers into the related router and  gater together such as  userRouter, videoRouter.  GlobalRouter is the router that gather except for  userRouter, videoRouter

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookeParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from './router';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
+import globalRouter from './routers/globalRouter';
+
+
 
 const app = express();
 const port = 4000;
-
-const handleHome = (req, res) => res.send('hello from home');
-const handleProfile = (req, res) =>  res.send('you are on my profile');
 
 app.use(cookeParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(helmet());
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 
-app.get("/",  handleHome);
-app.get('/profile', handleProfile);
+app.use('/', globalRouter);
 app.use('/user', userRouter);
-
+app.use('/video', videoRouter);
 
 export default app;
