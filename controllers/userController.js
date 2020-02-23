1a1f492766c8719285a622bf77e6262ea9a86d71
sumn <dltjsals6356@gmail.com>
2020-02-23 22:05:41 +0900

    [locals] add pageTitle

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
-export const join = (req, res) => res.send("join");
-export const login = (req, res) => res.send("login");
-export const logout = (req, res) => res.send("logout");
-
-
+export const join = (req, res) => res.render("join", {pageTitle : "join"});
+export const login = (req, res) => res.render("login", {pageTitle : "login"});
+export const logout = (req, res) => res.render("logout", {pageTitle : "logout"});
+export const userDetail = (req, res) => res.render("userDetail", {pageTitle : "userDetail"});
+export const editProfile = (req, res) => res.render("editProfile", {pageTitle : "editProfile"});
+export const changePassword = (req, res) => res.render("changePassword", {pageTitle : "changePassword"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,7 @@
 //rener => home.pug 파일을 찾아서 내보내준다 
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Saerch");
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("videoDetail");
-export const editVideo = (req, res) => res.send("editVideo");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+export const home = (req, res) => res.render("home", {pageTitle : "Home"});
+export const search = (req, res) => res.render("search", {pageTitle : "Sarch"});
+export const upload = (req, res) => res.render("upload" ,{pageTitle : "upload"});
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle : "videoDetail"});
+export const editVideo = (req, res) => res.render("editVideo" ,{pageTitle : "editVideo"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle : "SarcdeleteVideoh"});
