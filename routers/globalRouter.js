129ae84c3f70d0adb1b5ba06e0218654562e2704
sumn <dltjsals6356@gmail.com>
2020-02-23 23:29:50 +0900

    [routers] routers 정리

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -6,8 +6,8 @@ import { join, login, logout } from "../controllers/userController";
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
--- routes.js
+++ routes.js
@@ -1,42 +1,39 @@
-//Global
-const HOME = '/';
-const JOIN = '/join';
-const LOGIN = '/login';
-const LOGOUT = '/logout';
-const SEARCH = '/search';
+// Global
+const HOME = "/";
+const SEARCH = "/search";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
 
 
-//users 
-const USERS  = '/';
-const USER_DETAIL = '/:id';
-const EDIT_PROFILE = '/edit-profile';
-const CHANGE_PASSWORD = '/change-password';
+// Users
+const USERS = "/users";
+const USER_DETAIL = "/:id";
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
 
-
-//videos
-const VIDEO = '/videos';
-const UPLOAD = '/upload';
-const VIDEO_DETAIL = '/:id';
-const EDIT_VIDEO = '/:id/edit';
-const DELETE_VIDEO = '/:id/delete';
-
-
+// Videos
+const VIDEOS = "/videos";
+const UPLOAD = "/upload";
+const VIDEO_DETAIL = "/:id";
+const EDIT_VIDEO = "/:id/edit";
+const DELETE_VIDEO = "/:id/delete";
 
 const routes = {
-    home: HOME,
-    join: JOIN,
-    login: LOGIN,
-    logout: LOGOUT,
-    search: SEARCH,
-    users: USERS,
-    userDetail: USER_DETAIL,
-    editProfile: EDIT_PROFILE,
-    changePassword: CHANGE_PASSWORD,
-    videos: VIDEO,
-    upload: UPLOAD,
-    videoDetail: VIDEO_DETAIL,
-    editVideo: EDIT_VIDEO,
-    deleteVideo: DELETE_VIDEO,
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEO_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO
 };
 
 export default routes;
