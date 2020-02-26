1bf21429e757859fd0823b90fe831c02aed15594
sumn <dltjsals6356@gmail.com>
2020-02-27 07:25:52 +0900

    go home when logging / upload profile logout Links after logging

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+import routes from "../routes";
+
 export const getJoin = (req, res) => {
     res.render("join", {pageTitle : "join"});
 };
@@ -15,7 +17,13 @@ export const postJoin = (req, res) => {
         res.redirect(routes.home);
     }   
 };
-export const login = (req, res) => res.render("login", {pageTitle : "login"});
+
+
+export const getLogin = (req, res) => res.render("login", {pageTitle : "login"});
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
 export const logout = (req, res) => res.render("logout", {pageTitle : "logout"});
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle : "userDetail"});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle : "editProfile"});
--- middleward.js
+++ middleward.js
@@ -3,6 +3,9 @@ import routes from "./routes";
 export const  localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTue";
     res.locals.routes = routes;
-    //next() = 다음함수로 넘어간다 (코드사이에 있어서 next(써줘야함 ))
+    res.locals.user = {
+        isAuthenticated: true,
+        id: 1
+    };
     next();
 };
