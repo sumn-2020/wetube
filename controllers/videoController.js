dece16e04442b00efa667bde1a674268d58b6b23
sumn <dltjsals6356@gmail.com>
2020-02-27 00:25:46 +0900

    [searchingBy] get term information from url(parm)

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,12 @@
 //rener => home.pug 파일을 찾아서 내보내준다 
 export const home = (req, res) => res.render("home", {pageTitle : "Home"});
-export const search = (req, res) => res.render("search", {pageTitle : "Sarch"});
+export const search = (req, res) => {
+    //const searchingBy = req.query.term;
+    const {query: { term: searchingBy }} = req;
+    
+    //res.render("search", {pageTitle : "Sarch", searchingBy: searchingBy});
+    res.render("search", {pageTitle : "Sarch", searchingBy});
+}
 export const upload = (req, res) => res.render("upload" ,{pageTitle : "upload"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle : "videoDetail"});
 export const editVideo = (req, res) => res.render("editVideo" ,{pageTitle : "editVideo"});
