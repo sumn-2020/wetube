56e3623d574fced8b9f0d424669b3040a44fe559
sumn <dltjsals6356@gmail.com>
2020-02-27 07:43:17 +0900

    go home after logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,7 +24,14 @@ export const postLogin = (req, res) => {
     res.redirect(routes.home);
 };
 
-export const logout = (req, res) => res.render("logout", {pageTitle : "logout"});
+export const logout = (req, res) => {
+    //To do : process Log out 
+    res.redirect(routes.home);
+}
+ 
+
+
+
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle : "userDetail"});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle : "editProfile"});
 export const changePassword = (req, res) => res.render("changePassword", {pageTitle : "changePassword"});
