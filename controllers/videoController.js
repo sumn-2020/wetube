582ec15325a95fdeb85dc2d12065f7cf586bdf34
sumn <dltjsals6356@gmail.com>
2020-02-27 01:59:08 +0900

    show title and description of videos using fake db (each ~ in ~ )

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,11 @@
-//rener => home.pug 파일을 찾아서 내보내준다 
-export const home = (req, res) => res.render("home", {pageTitle : "Home"});
+import {videos} from "../db"
+export const home = (req, res) => {
+    res.render("home", {pageTitle : "Home", videos});
+};
 export const search = (req, res) => {
-    //const searchingBy = req.query.term;
     const {query: { term: searchingBy }} = req;
-    
-    //res.render("search", {pageTitle : "Sarch", searchingBy: searchingBy});
     res.render("search", {pageTitle : "Sarch", searchingBy});
-}
+};
 export const upload = (req, res) => res.render("upload" ,{pageTitle : "upload"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle : "videoDetail"});
 export const editVideo = (req, res) => res.render("editVideo" ,{pageTitle : "editVideo"});
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 12345678,
+        title: 'Video 1',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 4596787,
+            name: "tester",
+            email: "abcd@naver.com"
+        }
+    },
+    {
+        id: 89898989,
+        title: 'Video 2',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 4596787,
+            name: "tester",
+            email: "abcd@naver.com"
+        }
+    }, 
+    {
+        id: 89849490212,
+        title: 'Video 3',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 4596787,
+            name: "tester",
+            email: "abcd@naver.com"
+        }
+    }, 
+    {
+        id: 885221,
+        title: 'Video 4',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 4596787,
+            name: "tester",
+            email: "abcd@naver.com"
+        }
+    },
+];
