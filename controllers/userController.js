9bcb9de686fbbbbbbe8011a53caaf14156944c86
sumn <dltjsals6356@gmail.com>
2020-02-27 07:01:39 +0900

    password is same with password2, go to home, otherwise status error 400

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,20 @@
-export const join = (req, res) => res.render("join", {pageTitle : "join"});
+export const getJoin = (req, res) => {
+    res.render("join", {pageTitle : "join"});
+};
+export const postJoin = (req, res) => {
+    //console.log(req.body);
+    const {
+        body: { name, email, password, password2 }
+    } = req;
+    if (password !== password2) {
+        res.status(400); 
+        res.render("join", {pageTitle : "join"});
+    }else {
+        //To Do: Register User 
+        //To do: Log user in
+        res.redirect(routes.home);
+    }   
+};
 export const login = (req, res) => res.render("login", {pageTitle : "login"});
 export const logout = (req, res) => res.render("logout", {pageTitle : "logout"});
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle : "userDetail"});
