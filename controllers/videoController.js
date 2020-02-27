5672c6cfd839680402f9b9956aa3fd9d119bdc81
sumn <dltjsals6356@gmail.com>
2020-02-27 20:06:41 +0900

    getupload, postupload

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
-import {videos} from "../db"
+import {videos} from "../db";
+import routes from "../routes";
+
 export const home = (req, res) => {
     res.render("home", {pageTitle : "Home", videos});
 };
@@ -6,7 +8,15 @@ export const search = (req, res) => {
     const {query: { term: searchingBy }} = req;
     res.render("search", {pageTitle : "Sarch", searchingBy, videos});
 };
-export const upload = (req, res) => res.render("upload" ,{pageTitle : "upload"});
+export const getUpload = (req, res) => res.render("upload" ,{pageTitle : "upload"});
+export const postUpload = (req, res) => {
+    const {
+      body: { file, title, description }
+    } = req;
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(324393));
+  };
+
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle : "videoDetail"});
 export const editVideo = (req, res) => res.render("editVideo" ,{pageTitle : "editVideo"});
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle : "SarcdeleteVideoh"});
